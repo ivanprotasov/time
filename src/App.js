2ae7b37468d9a13b80671b3ac76ac062318ad157
IvanPratasau <Ivan_Pratasau@epam.com>
2018-11-16 17:57:15 +0300

    Base routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
 import { Button } from 'reactstrap';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Switch, Route, NavLink, Redirect } from 'react-router-dom';
 
 type Props = {
     disabled?: boolean
@@ -13,100 +13,95 @@ type Props = {
 class App extends Component<Props> {
     render() {
         return (
-            <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/topics">Topics</Link>
-                        </li>
-                    </ul>
+            <div>
+                <ul>
+                    <li>
+                        <NavLink to="/world-time">World time</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/alarm">Alarm</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/sleeping-mode">Sleeping mode</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/stopwatch">Stopwatch</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/timer">Timer</NavLink>
+                    </li>
+                </ul>
 
-                    <hr />
+                <hr />
 
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/topics" component={Topics} />
+                <Switch>
+                    <Route path="/world-time" component={WorldTime} />
+                    <Route path="/sleeping-mode" component={SleepingMode} />
+                    <Route path="/stopwatch" component={Stopwatch} />
+                    <Route path="/timer" component={Timer} />
+                    <Route path="/alarm" component={Alarm} />
+                    <Route render={() => <Redirect to="/alarm" />} />
+                </Switch>
 
-                    <div className={styles['App']}>
-                        <header className={styles['App-header']}>
-                            <img
-                                src={logo}
-                                className={styles['App-logo']}
-                                alt="logo"
-                            />
-                            <p>Saved1</p>
-                            <a
-                                className={styles['App-link']}
-                                href="https://reactjs.org"
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                Learn React
-                            </a>
-                            <Button>Click</Button>
-                        </header>
-                    </div>
-                </div>
-            </Router>
+                {/*<div className={styles['App']}>*/}
+                {/*<header className={styles['App-header']}>*/}
+                {/*<img*/}
+                {/*src={logo}*/}
+                {/*className={styles['App-logo']}*/}
+                {/*alt="logo"*/}
+                {/*/>*/}
+                {/*<p>Saved1</p>*/}
+                {/*<a*/}
+                {/*className={styles['App-link']}*/}
+                {/*href="https://reactjs.org"*/}
+                {/*target="_blank"*/}
+                {/*rel="noopener noreferrer">*/}
+                {/*Learn React*/}
+                {/*</a>*/}
+                {/*<Button>Click</Button>*/}
+                {/*</header>*/}
+                {/*</div>*/}
+            </div>
         );
     }
 }
 
-function Home() {
+function WorldTime() {
     return (
         <div>
-            <h2>Home</h2>
+            <h2>World time</h2>
         </div>
     );
 }
 
-function About() {
+function Alarm() {
     return (
         <div>
-            <h2>About</h2>
+            <h2>Alarm</h2>
         </div>
     );
 }
 
-function Topics({ match }) {
+function SleepingMode() {
     return (
         <div>
-            <h2>Topics</h2>
-            <ul>
-                <li>
-                    <Link to={`${match.url}/rendering`}>
-                        Rendering with React
-                    </Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/components`}>Components</Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/props-v-state`}>
-                        Props v. State
-                    </Link>
-                </li>
-            </ul>
-
-            <Route path={`${match.path}/:topicId`} component={Topic} />
-            <Route
-                exact
-                path={match.path}
-                render={() => <h3>Please select a topic.</h3>}
-            />
+            <h2>Sleeping mode</h2>
         </div>
     );
 }
 
-function Topic({ match }) {
+function Stopwatch() {
     return (
         <div>
-            <h3>{match.params.topicId}</h3>
+            <h2>Stopwatch</h2>
+        </div>
+    );
+}
+
+function Timer() {
+    return (
+        <div>
+            <h2>Timer</h2>
         </div>
     );
 }
